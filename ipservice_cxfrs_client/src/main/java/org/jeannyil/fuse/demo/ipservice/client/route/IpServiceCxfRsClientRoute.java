3755855834adc62e1bee16b17d9dadd6b03ccdd6
jeanNyil <jean.nyilimbibi@gmail.com>
2021-11-02 14:30:15 +0100

    Updated README

--- ipservice_cxfrs_client/src/main/java/org/jeannyil/fuse/demo/ipservice/client/route/IpServiceCxfRsClientRoute.java
+++ ipservice_cxfrs_client/src/main/java/org/jeannyil/fuse/demo/ipservice/client/route/IpServiceCxfRsClientRoute.java
@@ -11,30 +11,30 @@ public class IpServiceCxfRsClientRoute extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		// Error handling using the Default
-        onException(org.everit.json.schema.ValidationException.class)
-                .handled(false) // in order to allow the FileComponent moveFailed execution
-                .logStackTrace(true)
-                .logHandled(true)
-                .logExhausted(false)
-                .log(LoggingLevel.ERROR, "The content of [${file:name}] file is NOT VALID JSON: ${exception}")
-                // Send the error message to the JBoss A-MQ broker destination
-                .transform(simple("The content of [${file:name}] file is NOT VALID JSON: ${exception}"))
-				.removeHeaders("*")
-                .setHeader("ProcessedFile", exchangeProperty("ProcessedFile"))
-                .to(ExchangePattern.InOnly,"amq:{{error.amq.destination}}?timeToLive={{output.message.ttl.inms}}");
-        onException(Exception.class)
-                .handled(false) // in order to allow the FileComponent moveFailed execution
-                .logStackTrace(true)
-                .logExhausted(true)
-                .logHandled(true)
-                // Send the error message to the JBoss A-MQ broker destination
-                .transform(simple("Unexpected exception while processing [${exchangeProperty.ProcessedFile}]: ${exception.stacktrace}"))
-				.removeHeaders("*")
-                .setHeader(JMSPropertiesEnum.PROCESSEDFILE.toString(),
-                        exchangeProperty(JMSPropertiesEnum.PROCESSEDFILE.toString()))
-                .setHeader(JMSPropertiesEnum.PROCESSEDPARAMETERS.toString(),
-                        exchangeProperty(JMSPropertiesEnum.PROCESSEDPARAMETERS.toString()))
-                .to(ExchangePattern.InOnly,"amq:{{error.amq.destination}}?timeToLive={{output.message.ttl.inms}}");
+                onException(org.everit.json.schema.ValidationException.class)
+                        .handled(false) // in order to allow the FileComponent moveFailed execution
+                        .logStackTrace(true)
+                        .logHandled(true)
+                        .logExhausted(false)
+                        .log(LoggingLevel.ERROR, "The content of [${file:name}] file is NOT VALID JSON: ${exception}")
+                        // Send the error message to the JBoss A-MQ broker destination
+                        .transform(simple("The content of [${file:name}] file is NOT VALID JSON: ${exception}"))
+                                .removeHeaders("*")
+                        .setHeader("ProcessedFile", exchangeProperty("ProcessedFile"))
+                        .to(ExchangePattern.InOnly,"amq:{{error.amq.destination}}?timeToLive={{output.message.ttl.inms}}");
+                onException(Exception.class)
+                        .handled(false) // in order to allow the FileComponent moveFailed execution
+                        .logStackTrace(true)
+                        .logExhausted(true)
+                        .logHandled(true)
+                        // Send the error message to the JBoss A-MQ broker destination
+                        .transform(simple("Unexpected exception while processing [${exchangeProperty.ProcessedFile}]: ${exception.stacktrace}"))
+                                        .removeHeaders("*")
+                        .setHeader(JMSPropertiesEnum.PROCESSEDFILE.toString(),
+                                exchangeProperty(JMSPropertiesEnum.PROCESSEDFILE.toString()))
+                        .setHeader(JMSPropertiesEnum.PROCESSEDPARAMETERS.toString(),
+                                exchangeProperty(JMSPropertiesEnum.PROCESSEDPARAMETERS.toString()))
+                        .to(ExchangePattern.InOnly,"amq:{{error.amq.destination}}?timeToLive={{output.message.ttl.inms}}");
 
 		/**
 		 *  Route that consumes a file containing a list of input parameter for the RESTful IpService in JSON format
@@ -61,35 +61,35 @@ public class IpServiceCxfRsClientRoute extends RouteBuilder {
 				"&readLockTimeout=30000&readLockCheckInterval=10000" +
 				"&extendedAttributes=basic:creationTime&include=.*.json" +
 				"&moveFailed={{json.file.error.path}}")
-                .routeId("{{camel.name.route}}")
-                .log(LoggingLevel.INFO, "Processing consumed JSON file [${file:name}]...")
-                .log(LoggingLevel.INFO, "File content: ${body}")
-                .setProperty(JMSPropertiesEnum.PROCESSEDFILE.toString(), simple("${file:name}"))
+                        .routeId("{{camel.name.route}}")
+                        .log(LoggingLevel.INFO, "Processing consumed JSON file [${file:name}]...")
+                        .log(LoggingLevel.INFO, "File content: ${body}")
+                        .setProperty(JMSPropertiesEnum.PROCESSEDFILE.toString(), simple("${file:name}"))
 
-                // Validate JSON content against the expected JSON schema
-                .setProperty("jsonSchema", simple("Schemas/inputMessageSchema.json"))
-                .bean("jsonValidatorBean", "validateJsonWithEverit(${body},${header.jsonSchema})")
+                        // Validate JSON content against the expected JSON schema
+                        .setProperty("jsonSchema", simple("Schemas/inputMessageSchema.json"))
+                        .bean("jsonValidatorBean", "validateJsonWithEverit(${body},${header.jsonSchema})")
 
-                // Use of the splitter EIP to call the IP Geolocation RESTful service for each parameters set
-                .split().jsonpath("$..parameters.*").streaming()
-                    .log(LoggingLevel.INFO, "Split #${header.CamelSplitIndex} parameters: ${body}")
-                    .setProperty(JMSPropertiesEnum.PROCESSEDPARAMETERS.toString(), bodyAs(String.class))
-                    .removeHeaders("*") // Reset all the exchange message headers before proceeding
-                    // Prepare IP Geolocation RESTful service request
-                    .process("ipServiceGetGeoLocationRequestProcessor")
-                    .log(LoggingLevel.INFO, "Calling the IP Service getGeoLocation operation...")
-                    .to("cxfrs:bean:ipServiceRsClient")
-                    .log(LoggingLevel.INFO, "IP Service getGeoLocation operation response : ${body}")
-                    // Send the response to a JBoss A-MQ broker destination
-                    .removeHeaders("*")
-                    .setHeader(JMSPropertiesEnum.PROCESSEDFILE.toString(),
-							exchangeProperty(JMSPropertiesEnum.PROCESSEDFILE.toString()))
-                    .setHeader(JMSPropertiesEnum.PROCESSEDPARAMETERS.toString(),
-							exchangeProperty(JMSPropertiesEnum.PROCESSEDPARAMETERS.toString()))
-                    .to(ExchangePattern.InOnly, "amq:{{notif.amq.destination}}?timeToLive={{output.message.ttl.inms}}")
-                .end()
+                        // Use of the splitter EIP to call the IP Geolocation RESTful service for each parameters set
+                        .split().jsonpath("$..parameters.*").streaming()
+                                .log(LoggingLevel.INFO, "Split #${header.CamelSplitIndex} parameters: ${body}")
+                                .setProperty(JMSPropertiesEnum.PROCESSEDPARAMETERS.toString(), bodyAs(String.class))
+                                .removeHeaders("*") // Reset all the exchange message headers before proceeding
+                                // Prepare IP Geolocation RESTful service request
+                                .process("ipServiceGetGeoLocationRequestProcessor")
+                                .log(LoggingLevel.INFO, "Calling the IP Service getGeoLocation operation...")
+                                .to("cxfrs:bean:ipServiceRsClient")
+                                .log(LoggingLevel.INFO, "IP Service getGeoLocation operation response : ${body}")
+                                // Send the response to a JBoss A-MQ broker destination
+                                .removeHeaders("*")
+                                .setHeader(JMSPropertiesEnum.PROCESSEDFILE.toString(),
+                                        exchangeProperty(JMSPropertiesEnum.PROCESSEDFILE.toString()))
+                                .setHeader(JMSPropertiesEnum.PROCESSEDPARAMETERS.toString(),
+                                        exchangeProperty(JMSPropertiesEnum.PROCESSEDPARAMETERS.toString()))
+                                .to(ExchangePattern.InOnly, "amq:{{notif.amq.destination}}?timeToLive={{output.message.ttl.inms}}")
+                        .end()
 
-                .log(LoggingLevel.INFO, "Processing JSON file [${exchangeProperty.ProcessedFile}] DONE!")
-        ;
+                        .log(LoggingLevel.INFO, "Processing JSON file [${exchangeProperty.ProcessedFile}] DONE!")
+                ;
 	}
 }
